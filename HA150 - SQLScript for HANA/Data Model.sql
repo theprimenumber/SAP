d901d0fd618ff9f64f754c921eb43cc9c88b3a17
theprimenumber <154083208+theprimenumber@users.noreply.github.com>
2025-04-25 11:49:20 +0530

    Updated Data Model.sql

--- HA150 - SQLScript for HANA/Data Model.sql
+++ HA150 - SQLScript for HANA/Data Model.sql
@@ -12,6 +12,10 @@
 -- There is a list of vehicles that have been reported stolen.
 -- Owners, who have at least three vehicles registered, are assigned to one or multiple contacts.
 
+-- NOTE: If facing any issue due to Auto Commit DML is ON, wrap the code in a Anonymous Procedure block, so that DML Commit will be Off.
+
+DO
+BEGIN
 -- Table Definition: Official
 CREATE COLUMN TABLE Official(
   PNR VARCHAR(3) PRIMARY KEY,
@@ -19,19 +23,19 @@ CREATE COLUMN TABLE Official(
   Overtime INTEGER,
   Salary VARCHAR(3),
   Manager VARCHAR(3),
-  FOREIGN KEY(Manager) REFERENCES Official(PNR) INITIALLY DEFERRED,
+  FOREIGN KEY(Manager) REFERENCES Official(PNR) INITIALLY DEFERRED
 );
 
 -- Data Setup for Table 'Official'
 INSERT INTO Official VALUES ('P01','Mr A','10','A09','P04');
 INSERT INTO Official VALUES ('P02','Mr B','10','A10','P04');
 INSERT INTO Official VALUES ('P03','Ms C','20','A09','P04');
-INSERT INTO Official VALUES ('P04','Ms D',,'A12','P09');
+INSERT INTO Official VALUES ('P04','Ms D',NULL,'A12','P09');
 INSERT INTO Official VALUES ('P05','Mr E','10','A08','P08');
 INSERT INTO Official VALUES ('P06','Mr F','18','A09','P08');
 INSERT INTO Official VALUES ('P07','Ms G','22','A11','P08');
-INSERT INTO Official VALUES ('P08','Ms H',,'A13','P09');
-INSERT INTO Official VALUES ('P09','Mr I',,'A14',);
+INSERT INTO Official VALUES ('P08','Ms H',NULL,'A13','P09');
+INSERT INTO Official VALUES ('P09','Mr I',NULL,'A14',NULL);
 
 -- Owner Registration is done in table 'Owner'. Note that owners who are people have a birthday, but owners that are companies do not (There is no mechanism for it, so it should be on common sense).
 -- Table Definition: Owner
@@ -39,17 +43,17 @@ CREATE COLUMN TABLE Owner(
   OwnerID VARCHAR(3) PRIMARY KEY,
   Name VARCHAR(20),
   Birthday DATE,
-  City VARCHAR(20),
+  City VARCHAR(20)
 );
 
 -- Data Setup for Table 'Owner'
 INSERT INTO Owner VALUES('H01','Ms T','1934-06-20','Wiesloch');
 INSERT INTO Owner VALUES('H02','Ms U','1966-05-11','Hockenheim');
-INSERT INTO Owner VALUES('H03','SAP AG',,'Walldorf');
-INSERT INTO Owner VALUES('H04','HDM AG',,'Heidelberg');
+INSERT INTO Owner VALUES('H03','SAP AG',NULL,'Walldorf');
+INSERT INTO Owner VALUES('H04','HDM AG',NULL,'Heidelberg');
 INSERT INTO Owner VALUES('H05','Mr V','1952-04-21','Leimen');
 INSERT INTO Owner VALUES('H06','Ms W','1957-06-01','Wiesloch');
-INSERT INTO Owner VALUES('H07','IKEA',,'Walldorf');
+INSERT INTO Owner VALUES('H07','IKEA',NULL,'Walldorf');
 INSERT INTO Owner VALUES('H08','Mr X','1986-08-30','Walldorf');
 INSERT INTO Owner VALUES('H09','Ms Y','1986-02-10','Sinsheim');
 INSERT INTO Owner VALUES('H10','Mr Z','1986-02-03','Ladenburg');
@@ -58,12 +62,12 @@ INSERT INTO Owner VALUES('H10','Mr Z','1986-02-03','Ladenburg');
 -- Table Definition: Car
 CREATE COLUMN TABLE Car(
   CarID VARCHAR(3) PRIMARY KEY,
-  PlateNumber VARCHAR(10),
+  PlateNumber VARCHAR(10) UNIQUE,
   Brand VARCHAR(20),
   Color VARCHAR(10),
   HP INTEGER,
   Owner VARCHAR(3),
-  FOREIGN KEY(Owner) REFERENCES Owner(OwnerID) INITIALLY IMMEDIATE,
+  FOREIGN KEY(Owner) REFERENCES Owner(OwnerID) INITIALLY IMMEDIATE
 );
 
 -- Data Setup for table 'Car'
@@ -82,19 +86,19 @@ INSERT INTO Car VALUES('F12','HD-XY 4711','Skoda','red','105','H04');
 INSERT INTO Car VALUES('F13','HD-IK 1001','Renault','red','136','H07');
 INSERT INTO Car VALUES('F14','HD-MM 1977','Mercedes','white','170','H03');
 INSERT INTO Car VALUES('F15','HD-MB 3030','Skoda','black','136','H03');
-INSERT INTO Car VALUES('F16',,'Opel','green','120',);
+INSERT INTO Car VALUES('F16',NULL,'Opel','green','120',NULL);
 INSERT INTO Car VALUES('F17','HD-Y 333','Audi','orange','184','H09');
 INSERT INTO Car VALUES('F18','HD-MQ 2006','Renault','red','90','H03');
 INSERT INTO Car VALUES('F19','HD-VW 2012','VW','black','125','H01');
-INSERT INTO Car VALUES('F20',,'Audi','green','184',);
+INSERT INTO Car VALUES('F20',NULL,'Audi','green','184',NULL);
 
 -- The Contact database table relates a car owner owning more than two cars to the officials who are the respective contact persons. (There is no mechanism to verify for it, but it is possible by grouping)
 -- Table Definition: Contact
 CREATE COLUMN TABLE Contact(
   PersNumber VARCHAR(3),
   Owner VARCHAR(3),
-  FOREIGN KEY(PersNumber) REFERENCES Offical(PNR) INITIALLY IMMEDIATE,
-  FOREIGN KEY(Owner)      REFERENCES Owner(OwnerID) INITIALLY IMMEDIATE,
+  FOREIGN KEY(PersNumber) REFERENCES Official(PNR) INITIALLY IMMEDIATE,
+  FOREIGN KEY(Owner)      REFERENCES Owner(OwnerID) INITIALLY IMMEDIATE
 );
 
 -- Data Setup for Table 'Contact'
@@ -112,10 +116,12 @@ INSERT INTO Contact VALUES('P09','H03');
 CREATE COLUMN TABLE Stolen(
   PlateNumber VARCHAR(10),
   Reported_At DATE,
-  FOREIGN KEY(PlateNumber) REFERENCES Car(PlateNumber) INITIALLY IMMEDIATE,
+  FOREIGN KEY(PlateNumber) REFERENCES Car(PlateNumber) INITIALLY IMMEDIATE
 );
 
 -- Data Setup for Table 'Stolen'
 INSERT INTO Stolen VALUES('HD-VW 1999','2012-06-20');
 INSERT INTO Stolen VALUES('HD-V 106','2012-06-01');
 INSERT INTO Stolen VALUES('HD-Y 333','2012-05-21');
+
+END;
